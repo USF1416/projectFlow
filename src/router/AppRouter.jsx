97bd5e3aa77d53858495857624ece938dacdc7ff
USF1416 <youssef.hamri.etudiant@gmail.com>
2026-01-20 19:19:04 +0100

    refeonte complet du projet

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,18 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import LoginPage from "./pages/LoginPage.jsx";
+import DashboardPage from "./pages/DashboardPage.jsx";
+import ProjectsPage from "./pages/ProjectsPage.jsx";
+import BoardPage from "./pages/BoardPage.jsx";
+
+export default function AppRouter() {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/dashboard" element={<DashboardPage />} />
+      <Route path="/projects" element={<ProjectsPage />} />
+      <Route path="/board" element={<BoardPage />} />
+      <Route path="*" element={<Navigate to="/login" replace />} />
+    </Routes>
+  );
+}
